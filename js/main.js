d1d4f1fba29518e77fc31433d2420de10cfbe655
markiparki <mail@markusarendt.de>
2022-05-23 20:32:29 +0200

    project moved

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function() {
+  gumshoe.init({
+    offset: 50,
+    activeClass: 'active'
+  });
+
+  smoothScroll.init({
+    selector: 'a',
+    speed: 500,
+    easing: 'easeInOutCubic',
+    offset: 50,
+    updateURL: false
+  });
+
+}).call(this);
+
+ //# sourceMappingURL=main.js.map
